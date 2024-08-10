0b5d5e3d2ad8bf15c43f8e21ac89ddd77a98f328
ddr4869 <bikdu@naver.com>
2024-08-10 18:38:12 +0900

    [fix] apply food loader

--- src/main/java/dino/junction/domain/food/FoodLoader.java
+++ src/main/java/dino/junction/domain/food/FoodLoader.java
@@ -1,21 +1,11 @@
 package dino.junction.domain.food;
 
-import aj.org.objectweb.asm.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import dino.junction.domain.food.model.entity.FoodEntity;
 import dino.junction.domain.food.service.FoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -24,7 +14,7 @@ public class FoodLoader implements CommandLineRunner {
     private final FoodService foodService;
     @Override
     public void run(String... args) throws Exception {
-        String jsonFilePath = "src/";  // 실제 JSON 파일 경로로 변경하세요
+        String jsonFilePath = "src/main/resources/food.json";
         try {
             foodService.saveFoodsFromJson(jsonFilePath);
             System.out.println("Food data loaded successfully.");
